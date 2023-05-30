111c7431e38b3677ab687a9b6133551b970ff2c6
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-05-30 11:41:04 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -15,7 +15,7 @@ const Notification = () => {
         if (notificationsData) {
           const notificationsArray = Object.values(notificationsData);
           setNotifications(notificationsArray);
-          setPiscando(true);
+          setPiscando(true); // Ativar o efeito de piscar quando as notificações forem atualizadas
         }
       } catch (error) {
         console.log('Erro ao buscar notificações:', error);
@@ -27,14 +27,19 @@ const Notification = () => {
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setPiscando(false);
-    }, 8000);
+      setPiscando(false); // Desativar o efeito de piscar após 1 segundo
+    }, 1000);
 
     return () => clearTimeout(timeout);
   }, [notifications]);
 
   return (
-    <div className={`nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96 ${piscando ? 'piscando' : ''}`}>
+    <div
+      className={`nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96`}
+      style={{
+        animation: `${piscando ? 'piscar 1s infinite' : 'none'}`,
+      }}
+    >
       <div className="flex justify-between items-center">
         <div>
           <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
