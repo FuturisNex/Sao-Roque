092f3d94a4d07dffa1fe5b5e532610ed5ae1ad59
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-05-30 11:13:49 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -25,18 +25,15 @@ const Notification = () => {
 
   return (
     <div className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
-      <div className="flex justify-between items-center">
-        <div className="flex gap-3">
-          <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
-          <button type="button" className="text-white text-xs rounded p-1 px-2 bg-orange-theme">
-            {notifications.length} Notificação{notifications.length !== 1 ? 'ões' : ''}
-          </button>
-        </div>
-        <Button icon={<MdOutlineCancel />} color="rgb(153, 171, 180)" bgHoverColor="light-gray" size="2xl" borderRadius="50%" />
+      <div className="flex flex-col">
+        <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
+        <button type="button" className="text-white text-xs rounded p-1 px-2 bg-orange-theme mt-2">
+          {notifications.length} Notificação{notifications.length !== 1 ? 'ões' : ''}
+        </button>
       </div>
       <div className="mt-5">
         {notifications.map((notification, index) => (
-          <div key={index} className="flex items-center leading-8 gap-5 border-b-1 border-color p-3">
+          <div key={index} className="flex flex-col gap-1 border-b-1 border-color p-3">
             <p className="font-semibold dark:text-gray-200">{notification.Titulo}</p>
             <p className="text-gray-500 text-sm dark:text-gray-400">{notification.Descricao}</p>
           </div>
@@ -47,6 +44,14 @@ const Notification = () => {
           </div>
         )}
       </div>
+      <Button
+        icon={<MdOutlineCancel />}
+        color="rgb(153, 171, 180)"
+        bgHoverColor="light-gray"
+        size="2xl"
+        borderRadius="50%"
+        className="mt-5"
+      />
     </div>
   );
 };
