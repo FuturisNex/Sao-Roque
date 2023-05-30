c70b588354e7b4f9a0a3f655716074cb1e55fd3b
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-05-30 11:16:13 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -25,11 +25,20 @@ const Notification = () => {
 
   return (
     <div className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
-      <div className="flex flex-col">
-        <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
-        <button type="button" className="text-white text-xs rounded p-1 px-2 bg-orange-theme mt-2">
-          {notifications.length} Notificação{notifications.length !== 1 ? 'ões' : ''}
-        </button>
+      <div className="flex justify-between items-center">
+        <div>
+          <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
+          <button type="button" className="text-white text-xs rounded p-1 px-2 bg-orange-theme mt-2">
+            {notifications.length} Notificação{notifications.length !== 1 ? 'ões' : ''}
+          </button>
+        </div>
+        <Button
+          icon={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+        />
       </div>
       <div className="mt-5">
         {notifications.map((notification, index) => (
@@ -44,14 +53,6 @@ const Notification = () => {
           </div>
         )}
       </div>
-      <Button
-        icon={<MdOutlineCancel />}
-        color="rgb(153, 171, 180)"
-        bgHoverColor="light-gray"
-        size="2xl"
-        borderRadius="50%"
-        className="mt-5"
-      />
     </div>
   );
 };
