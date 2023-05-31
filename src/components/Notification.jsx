b2a0bad63d2e05efa65c3fd2a1bd074886fe2103
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-05-31 11:16:35 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -27,19 +27,14 @@ const Notification = () => {
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setPiscando(false); // Desativar o efeito de piscar após 5 segundo
-    }, 5000);
+      setPiscando(false); // Desativar o efeito de piscar após 1 segundo
+    }, 1000);
 
     return () => clearTimeout(timeout);
   }, [notifications]);
 
   return (
-   <div
-      className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96"
-      style={{
-        animation: `${piscando ? 'piscar 1s infinite' : 'none'}`,
-      }}
-    >
+    <div className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
       <div className="flex justify-between items-center">
         <div>
           <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
