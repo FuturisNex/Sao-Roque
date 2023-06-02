3f7e223b6f48a23b621d7f62ddadf204edbd1cd3
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-02 08:58:51 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -26,14 +26,6 @@ const Notification = () => {
     };
 
     fetchNotifications();
-
-    const interval = setInterval(() => {
-      setPiscando((prevState) => !prevState);
-    }, 500); // Define o intervalo de piscar em milissegundos (neste exemplo, 500ms)
-
-    return () => {
-      clearInterval(interval);
-    };
   }, []);
 
   return (
