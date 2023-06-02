497054af27c112e7303dfc80074b29146c2661bb
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-02 09:02:00 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
@@ -8,7 +8,7 @@ import avatar from '../data/avatar.png';
 import { Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
-const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+const NavButton = ({ title, customFunc, icon, color, dotColor, isPiscando }) => (
   <TooltipComponent content={title} position="BottomCenter">
     <button
       type="button"
@@ -18,7 +18,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
     >
       <span
         style={{ background: dotColor }}
-        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        className={`absolute inline-flex rounded-full h-2 w-2 right-2 top-2 ${isPiscando ? 'piscando' : ''}`}
       />
       {icon}
     </button>
@@ -27,6 +27,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 const Navbar = () => {
   const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
+  const [piscandoNotificacao, setPiscandoNotificacao] = useState(false);
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -46,13 +47,31 @@ const Navbar = () => {
     }
   }, [screenSize]);
 
+  useEffect(() => {
+    const fetchPiscarNotificacao = async () => {
+      try {
+        const snapshot = await database.ref('notificacao/noti').once('value');
+        const notificationsData = snapshot.val();
+        if (notificationsData) {
+          const notificationsArray = Object.values(notificationsData);
+          const isPiscando = notificationsArray.some(notification => notification.Piscar === true);
+          setPiscandoNotificacao(isPiscando);
+        }
+      } catch (error) {
+        console.log('Erro ao buscar notificações:', error);
+      }
+    };
+
+    fetchPiscarNotificacao();
+  }, []);
+
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
     <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
       <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
       <div className="flex">
-        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
+        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} isPiscando={piscandoNotificacao} />
         <TooltipComponent content="Profile" position="BottomCenter">
           <div
             className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
