44e284b42ebf230a82845257f1fc8f9171ab0cdc
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-12 14:21:05 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -6,7 +6,7 @@ import './Style/noti.css';
 
 const Notification = ({ navId }) => {
   const [notifications, setNotifications] = useState([]);
-  const [showNotificationDot, setShowNotificationDot] = useState(false);
+  const [piscando, setPiscando] = useState(false);
 
   useEffect(() => {
     const ref = database.ref('notificacao');
@@ -14,11 +14,13 @@ const Notification = ({ navId }) => {
     const handleNotificationAdded = (snapshot) => {
       const notification = snapshot.val();
       setNotifications((prevState) => [...prevState, notification]);
+      setPiscando(true);
     };
 
     const handleNotificationRemoved = (snapshot) => {
       const notification = snapshot.val();
       setNotifications((prevState) => prevState.filter((item) => item.id !== notification.id));
+      setPiscando(notifications.length > 1);
     };
 
     ref.child('noti').on('child_added', handleNotificationAdded);
@@ -30,18 +32,14 @@ const Notification = ({ navId }) => {
     };
   }, []);
 
-  useEffect(() => {
-    setShowNotificationDot(notifications.length > 0);
-  }, [notifications]);
-
   return (
-    <div className={`nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96 ${showNotificationDot ? 'has-dot' : ''}`} id={navId}>
+    <div className={`nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96 ${piscando ? 'piscando' : ''}`} id={navId}>
       <div className="flex justify-between items-center">
         <div>
           <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
           <button
             type="button"
-            className={`text-white text-xs rounded p-1 px-2 mt-2 ${showNotificationDot ? 'bg-red-theme' : ''}`}
+            className={`text-white text-xs rounded p-1 px-2 mt-2 ${piscando ? 'bg-red-theme' : ''}`}
           >
             {notifications.length} Notificação{notifications.length !== 1 ? 'ões' : ''}
           </button>
@@ -55,14 +53,13 @@ const Notification = ({ navId }) => {
         />
       </div>
       <div className="mt-5">
-        {notifications.length > 0 ? (
-          notifications.map((notification, index) => (
-            <div key={index} className="flex flex-col gap-1 border-b-1 border-color p-3">
-              <p className="font-semibold dark:text-gray-200">{notification.Titulo}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400">{notification.Descricao}</p>
-            </div>
-          ))
-        ) : (
+        {notifications.map((notification, index) => (
+          <div key={index} className="flex flex-col gap-1 border-b-1 border-color p-3">
+            <p className="font-semibold dark:text-gray-200">{notification.Titulo}</p>
+            <p className="text-gray-500 text-sm dark:text-gray-400">{notification.Descricao}</p>
+          </div>
+        ))}
+        {notifications.length === 0 && (
           <div className="flex items-center justify-center mt-3">
             <p className="text-gray-500 text-sm">Nenhuma notificação encontrada.</p>
           </div>
