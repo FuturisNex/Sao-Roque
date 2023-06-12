6fb769ae07230032c79b6bcdb6613ba1df5ea320
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-12 14:25:51 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import database from '../auth/firebase.js';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
@@ -27,32 +28,46 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 const Navbar = () => {
   const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
+  const [piscarStatus, setPiscarStatus] = useState(false);
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
-
     window.addEventListener('resize', handleResize);
-
     handleResize();
-
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   useEffect(() => {
-    if (screenSize <= 900) {
-      setActiveMenu(false);
-    } else {
-      setActiveMenu(true);
-    }
-  }, [screenSize]);
+    const ref = database.ref('notificacao/Piscar');
+
+    // Cria um listener para verificar o valor em tempo real
+    ref.on('value', snapshot => {
+      const value = snapshot.val();
+      setPiscarStatus(value === true);
+    });
+
+    // Remove o listener quando o componente é desmontado
+    return () => ref.off('value');
+  }, []);
 
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
     <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
-      <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
+      <NavButton
+        title="Menu"
+        customFunc={handleActiveMenu}
+        color={currentColor}
+        icon={<AiOutlineMenu />}
+      />
       <div className="flex">
-        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
+        <NavButton
+          title="Notification"
+          dotColor={piscarStatus ? 'rgb(254, 201, 15)' : 'transparent'}
+          customFunc={() => handleClick('notification')}
+          color={currentColor}
+          icon={<RiNotification3Line />}
+        />
         <TooltipComponent content="Profile" position="BottomCenter">
           <div
             className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
@@ -73,8 +88,8 @@ const Navbar = () => {
           </div>
         </TooltipComponent>
       </div>
-      {isClicked.notification && (<Notification />)}
-      {isClicked.userProfile && (<UserProfile />)}
+      {isClicked.notification && <Notification />}
+      {isClicked.userProfile && <UserProfile />}
     </div>
   );
 };
