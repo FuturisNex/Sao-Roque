493f33cdfb1678959b94e840bf00db724b328fbb
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-12 14:17:44 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import avatar from '../data/avatar.png';
 import { Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
-const NavButton = ({ title, customFunc, icon, color, dotColor, hasDot }) => (
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position="BottomCenter">
     <button
       type="button"
       onClick={() => customFunc()}
       style={{ color }}
-      className={`relative text-xl rounded-full p-3 hover:bg-light-gray ${hasDot ? 'has-dot' : ''}`}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
     >
-      {hasDot && (
-        <span
-          style={{ background: dotColor }}
-          className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-        />
-      )}
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />
       {icon}
     </button>
   </TooltipComponent>
 );
 
 const Navbar = () => {
-  const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, isClicked, setScreenSize, screenSize } = useStateContext();
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -49,18 +49,19 @@ const Navbar = () => {
 
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
+  const handleClick = (menu) => {
+    if (menu === 'notification') {
+      setShowNotification(!showNotification);
+    } else {
+      setShowNotification(false);
+    }
+  };
+
   return (
     <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
       <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
       <div className="flex">
-        <NavButton
-          title="Notification"
-          dotColor="rgb(254, 201, 15)"
-          customFunc={() => handleClick('notification')}
-          color={currentColor}
-          icon={<RiNotification3Line />}
-          hasDot={isClicked.notification}
-        />
+        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
         <TooltipComponent content="Profile" position="BottomCenter">
           <div
             className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
@@ -81,7 +82,7 @@ const Navbar = () => {
           </div>
         </TooltipComponent>
       </div>
-      {isClicked.notification && (<Notification />)}
+      {showNotification && (<Notification setShowNotification={setShowNotification} />)}
       {isClicked.userProfile && (<UserProfile />)}
     </div>
   );
