68ee90be7d32c06edb52a30ef77480a16c04c9dc
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-26 15:20:46 -0300

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
 import Button from './Button';
 
@@ -8,9 +8,22 @@ import avatar from '../data/avatar.png';
 
 const UserProfile = () => {
   const { currentColor } = useStateContext();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <div className="nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
+    <div className={`nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg ${windowWidth < 640 ? 'w-full' : 'w-96'}`}>
       <div className="flex justify-between items-center">
         <p className="font-semibold text-lg dark:text-gray-200">Perfil de Usuário</p>
         <Button
@@ -28,24 +41,31 @@ const UserProfile = () => {
           alt="user-profile"
         />
         <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> Usuário </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400"> Usuário Padrão </p>
+          <p className="font-semibold text-xl dark:text-gray-200">Usuário</p>
+          <p className="text-gray-500 text-sm dark:text-gray-400">Usuário Padrão</p>
         </div>
       </div>
       <div>
         {userProfileData.map((item, index) => (
-          <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
+          <div
+            key={index}
+            className={`flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer ${
+              windowWidth < 640 ? 'flex-col items-center' : ''
+            }`}
+          >
             <button
               type="button"
               style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              className=" text-xl rounded-lg p-3 hover:bg-light-gray"
+              className={`text-xl rounded-lg p-3 hover:bg-light-gray ${
+                windowWidth < 640 ? 'mb-4' : ''
+              }`}
             >
               {item.icon}
             </button>
 
             <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
+              <p className="font-semibold dark:text-gray-200">{item.title}</p>
+              <p className="text-gray-500 text-sm dark:text-gray-400">{item.desc}</p>
             </div>
           </div>
         ))}
@@ -60,7 +80,6 @@ const UserProfile = () => {
         />
       </div>
     </div>
-
   );
 };
 
