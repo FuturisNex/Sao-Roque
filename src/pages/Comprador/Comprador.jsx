23926512119fcccc98ae017cab01d2d15b94f4e4
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-26 15:00:43 -0300

    Update Comprador.jsx

--- src/pages/Comprador/Comprador.jsx
+++ src/pages/Comprador/Comprador.jsx
@@ -4,28 +4,53 @@ import { AiOutlineUser } from 'react-icons/ai';
 import { useStateContext } from '../../contexts/ContextProvider';
 import './Comprador.css';
 
-const buyers = ['ADAUTO', 'MARIVONE', 'NETA', 'SENA'];
+const buyers = [
+  { name: 'ADAUTO', icon: <AiOutlineUser size={30} color="white" /> },
+  { name: 'MARIVONE', icon: <AiOutlineUser size={30} color="white" /> },
+  { name: 'NETA', icon: <AiOutlineUser size={30} color="white" /> },
+  { name: 'SENA', icon: <AiOutlineUser size={30} color="white" /> },
+];
 
-const Comprador = () => {
+const BuyerCard = ({ buyer, handleClick }) => {
   const { currentMode, currentColor } = useStateContext();
 
   return (
-    <div className={`buyers-container ${currentMode === 'Dark' ? 'dark' : ''}`}>
-      <div className="buyers-grid">
+    <div
+      className={`buyer-card ${currentMode === 'Dark' ? 'dark' : ''}`}
+      style={{ backgroundColor: currentColor }}
+      onClick={() => handleClick(buyer.name)}
+    >
+      <div className="buyer-icon">{buyer.icon}</div>
+      <p>{buyer.name}</p>
+    </div>
+  );
+};
+
+const Comprador = () => {
+  const { currentMode } = useStateContext();
+
+  const handleBuyerClick = (buyer) => {
+    const url = {
+      ADAUTO: '/adauto',
+      MARIVONE: '/marivone',
+      NETA: '/neta',
+      SENA: '/sena',
+
+    }[buyer];
+
+    if (url) {
+      window.open(url, '_blank');
+    }
+  };
+
+  return (
+    <div className={`container ${currentMode === 'Dark' ? 'dark' : ''}`}>
+      <div className="buyers">
         {buyers.map((buyer, index) => (
-          <Link
-            key={index}
-            to={`/${buyer.toLowerCase()}`}
-            className={`buyer-card ${currentMode === 'Dark' ? 'dark' : ''}`}
-            style={{ backgroundColor: currentColor }}
-          >
-            <AiOutlineUser size={30} color="white" />
-            <p>{buyer}</p>
-          </Link>
+          <BuyerCard key={index} buyer={buyer} handleClick={handleBuyerClick} />
         ))}
       </div>
     </div>
   );
 };
-
 export default Comprador;
