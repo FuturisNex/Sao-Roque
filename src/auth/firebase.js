0540c0ec4748bae63d298478b4ffc2b3c2fa050a
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-29 16:41:02 -0300

    Update firebase.js

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,16 +1,18 @@
-import firebase from 'firebase/compat/app';
 import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBR7ZeYNiLbshvMe5powb4wNnT6p7xt1q8",
-  authDomain: "grupo-sao-roque.firebaseapp.com",
-  databaseURL: "https://grupo-sao-roque-default-rtdb.firebaseio.com",
-  projectId: "grupo-sao-roque",
-  storageBucket: "grupo-sao-roque.appspot.com",
-  messagingSenderId: "436605416235",
-  appId: "1:436605416235:web:6e9db798ded70ab7690b6e",
-  measurementId: "G-LMTWTP55TE"
+  apiKey: 'AIzaSyBR7ZeYNiLbshvMe5powb4wNnT6p7xt1q8',
+  authDomain: 'grupo-sao-roque.firebaseapp.com',
+  databaseURL: 'https://grupo-sao-roque-default-rtdb.firebaseio.com',
+  projectId: 'grupo-sao-roque',
+  storageBucket: 'grupo-sao-roque.appspot.com',
+  messagingSenderId: '436605416235',
+  appId: '1:436605416235:web:6e9db798ded70ab7690b6e',
 };
-
 const app = firebase.initializeApp(firebaseConfig);
+const database = app.database();
 const analytics = getAnalytics(app);
+
+export default database;
