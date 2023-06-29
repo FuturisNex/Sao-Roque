8480b5b884315266975350eaf1944d19f44f809e
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-29 15:45:47 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,84 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { MdOutlineCancel } from 'react-icons/md';
-import database from '../auth/firebase.js';
-import Button from './Button';
-import './Style/noti.css';
+import React, { useEffect, useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { getMessaging, onMessage } from 'firebase/compat/messaging';
+import database from '../auth/firebase.js'; // Importe a instância do Firebase
 
-const Notification = ({ navId }) => {
+const Notification = () => {
   const [notifications, setNotifications] = useState([]);
-  const [piscando, setPiscando] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
+    const messaging = getMessaging(database); // Use a instância do Firebase importada
 
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    const ref = database.ref('Notificacao');
-
-    const handleNotificationAdded = (snapshot) => {
-      const notification = snapshot.val();
+    onMessage(messaging, (payload) => {
+      const { notification } = payload;
       setNotifications((prevState) => [...prevState, notification]);
-      setPiscando(true);
-    };
-
-    const handleNotificationRemoved = (snapshot) => {
-      const notification = snapshot.val();
-      setNotifications((prevState) => prevState.filter((item) => item.id !== notification.id));
-      setPiscando(notifications.length > 1);
-    };
-
-    ref.child('Alerta').on('child_added', handleNotificationAdded);
-    ref.child('Alerta').on('child_removed', handleNotificationRemoved);
+    });
 
     return () => {
-      ref.child('Alerta').off('child_added', handleNotificationAdded);
-      ref.child('Alerta').off('child_removed', handleNotificationRemoved);
+      messaging.onMessage.unsubscribe();
     };
   }, []);
 
-  return (
-    <div className={`nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg ${isMobile ? 'w-full' : 'w-96'} ${piscando ? 'piscando' : ''}`} id={navId}>
-      <div className="flex justify-between items-center">
-        <div>
-          <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
-          <button
-            type="button"
-            className={`text-white text-xs rounded p-1 px-2 mt-2 ${piscando ? 'bg-red-theme' : ''}`}
-          >
-            {notifications.length} Notificação{notifications.length !== 1 ? 'ões' : ''}
-          </button>
-        </div>
-        <Button
-          icon={<MdOutlineCancel />}
-          color="rgb(153, 171, 180)"
-          bgHoverColor="light-gray"
-          size="2xl"
-          borderRadius="50%"
-        />
+  const handleClick = () => {
+    setNotifications([]);
+  };
+
+   return (
+    <div className="absolute bg-white shadow-lg rounded-xl text-14 z-20 overflow-y-auto max-h-96 w-60 top-14 right-0 mr-6 mt-1">
+      <div className="flex justify-between items-center p-2 border-b border-gray-300">
+        <h2 className="text-base font-semibold">Notificações</h2>
+        <button
+          type="button"
+          onClick={handleClick}
+          className="text-gray-500 hover:text-gray-700"
+        >
+          <MdClose />
+        </button>
       </div>
-      <div className="mt-5">
+      <div className="p-2">
         {notifications.map((notification, index) => (
-          <div key={index} className="flex flex-col gap-1 border-b-1 border-color p-3">
-            <p className="font-semibold dark:text-gray-200">{notification.titulo}</p>
-            <p className="text-gray-500 text-sm dark:text-gray-400">{notification.descricao}</p>
+          <div key={index} className="mb-2">
+            <p>{notification.body}</p>
           </div>
         ))}
-        {notifications.length === 0 && (
-          <div className="flex items-center justify-center mt-3">
-            <p className="text-gray-500 text-sm">Nenhuma notificação encontrada.</p>
-          </div>
-        )}
       </div>
     </div>
   );
