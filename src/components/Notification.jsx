86a2b1d095428b3617b285e5eb234c3812288a3a
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-30 17:32:46 -0300

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,56 +1,105 @@
-import React, { useEffect, useState } from 'react';
-import { MdClose } from 'react-icons/md';
-import { initializeApp } from 'firebase/app';
-import { getMessaging, onMessage } from 'firebase/messaging';
+import React, { useState, useEffect } from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import database from '../auth/firebase.js';
+import Button from './Button';
+import './Style/noti.css';
 
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: 'AIzaSyBR7ZeYNiLbshvMe5powb4wNnT6p7xt1q8',
-  authDomain: 'grupo-sao-roque.firebaseapp.com',
-  databaseURL: 'https://grupo-sao-roque-default-rtdb.firebaseio.com',
-  projectId: 'grupo-sao-roque',
-  storageBucket: 'grupo-sao-roque.appspot.com',
-  messagingSenderId: '436605416235',
-  appId: '1:436605416235:web:6e9db798ded70ab7690b6e',
-};
-
-// Inicialização do app Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
-const Notification = () => {
+const Notification = ({ navId }) => {
   const [notifications, setNotifications] = useState([]);
+  const [piscando, setPiscando] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Listener para receber notificações em tempo real
-    onMessage(messaging, (payload) => {
-      const { notification } = payload;
-      setNotifications((prevState) => [...prevState, notification]);
-    });
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
-  const handleClick = () => {
-    setNotifications([]);
+  useEffect(() => {
+    const ref = database.ref('Notificacoes');
+
+    const handleNotificationAdded = (snapshot) => {
+      const notification = snapshot.val();
+      setNotifications((prevState) => [...prevState, notification]);
+      setPiscando(true);
+
+      const expirationTimeInMillis = notification.expiracao * 24 * 60 * 60 * 1000;
+
+      setTimeout(() => {
+        setNotifications((prevState) => prevState.filter((item) => item.id !== notification.id));
+        setPiscando(prevState => prevState && notifications.length > 1);
+      }, expirationTimeInMillis);
+    };
+
+    const handleNotificationRemoved = (snapshot) => {
+      const notification = snapshot.val();
+      setNotifications((prevState) =>
+        prevState.filter((item) => item.id !== notification.id)
+      );
+      setPiscando(prevState => prevState && notifications.length > 1);
+    };
+
+    ref.on('child_added', handleNotificationAdded);
+    ref.on('child_removed', handleNotificationRemoved);
+
+    return () => {
+      ref.off('child_added', handleNotificationAdded);
+      ref.off('child_removed', handleNotificationRemoved);
+    };
+  }, []);
+
+  const handleRemoveNotification = (id) => {
+    database.ref(`Notificacoes/${id}`).remove();
   };
 
   return (
-    <div className="absolute bg-white shadow-lg rounded-xl text-14 z-20 overflow-y-auto max-h-96 w-60 top-14 right-0 mr-6 mt-1">
-      <div className="flex justify-between items-center p-2 border-b border-gray-300">
-        <h2 className="text-base font-semibold">Notificações</h2>
-        <button
-          type="button"
-          onClick={handleClick}
-          className="text-gray-500 hover:text-gray-700"
-        >
-          <MdClose />
-        </button>
+    <div className={`nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg ${isMobile ? 'w-full' : 'w-96'
+      } ${piscando ? 'piscando' : ''}`} id={navId}>
+      <div className="flex justify-between items-center">
+        <div>
+          <p className="font-semibold text-lg dark:text-gray-200">Notificações</p>
+          <button
+            type="button"
+            className={`text-white text-xs rounded p-1 px-2 mt-2 ${piscando ? 'bg-red-theme' : ''}`}
+          >
+            {notifications.length} Notificação{notifications.length !== 1 ? 'ões' : ''}
+          </button>
+        </div>
+        <Button
+          icon={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+        />
       </div>
-      <div className="p-2">
-        {notifications.map((notification, index) => (
-          <div key={index} className="mb-2">
-            <p>{notification.body}</p>
+      <div className="mt-5">
+        {notifications.map((notification) => (
+          <div key={notification.id} className="flex flex-col gap-1 border-b-1 border-color p-3">
+            <p className="font-semibold dark:text-gray-200">{notification.titulo}</p>
+            <p className="text-gray-500 text-sm dark:text-gray-400">{notification.descricao}</p>
+            <Button
+              onClick={() => handleRemoveNotification(notification.id)}
+              label="Remover"
+              size="sm"
+              color="red"
+              className="mt-2"
+            />
           </div>
         ))}
+        {notifications.length === 0 && (
+          <div className="flex items-center justify-center mt-3">
+            <p className="text-gray-500 text-sm">Nenhuma notificação encontrada.</p>
+          </div>
+        )}
       </div>
     </div>
   );
