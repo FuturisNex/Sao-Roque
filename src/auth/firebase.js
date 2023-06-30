6cb764f5afc601bb149033a5642b929018b88b60
Gustavo <128225301+GustavoAlpha@users.noreply.github.com>
2023-06-30 17:20:08 -0300

    Update firebase.js

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/database';
+import 'firebase/compat/messaging';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBR7ZeYNiLbshvMe5powb4wNnT6p7xt1q8',
@@ -10,7 +11,26 @@ const firebaseConfig = {
   messagingSenderId: '436605416235',
   appId: '1:436605416235:web:6e9db798ded70ab7690b6e',
 };
-const app = firebase.initializeApp(firebaseConfig);
-const database = app.database();
 
-export default database;
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+const requestFirebaseNotificationPermission = async () => {
+  try {
+    await Notification.requestPermission();
+    const token = await getToken(messaging);
+    console.log('Firebase Cloud Messaging token:', token);
+  } catch (error) {
+    console.error('Error requesting notification permission:', error);
+  }
+};
+
+const setupFirebaseMessaging = () => {
+  onMessage(messaging, (payload) => {
+    console.log('Received FCM message:', payload);
+    // Faça algo com a notificação recebida, como exibir um toast ou uma notificação no navegador
+    new Notification(payload.notification.title, { body: payload.notification.body });
+  });
+};
+
+export { app, messaging, requestFirebaseNotificationPermission, setupFirebaseMessaging };
