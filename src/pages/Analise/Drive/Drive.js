4537a52d25efb837aac3020c6aefa8b2b81f9b81
Gustavo <128225301+Gustavo-LionStar@users.noreply.github.com>
2023-10-17 16:24:58 -0300

    Update Drive.js

--- src/pages/Analise/Drive/Drive.js
+++ src/pages/Analise/Drive/Drive.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Drive.css';
 
 function Drive() {
-  const iframeUrl = "https://lookerstudio.google.com/embed/reporting/b3ed707c-2106-4a76-816e-b52138f230f6/page/p_bsxgg1805c";
+  const iframeUrl = 'https://lookerstudio.google.com/embed/reporting/b3ed707c-2106-4a76-816e-b52138f230f6/page/p_bsxgg1805c';
 
   const scaledIframeUrl = `${iframeUrl}&scale=0.5`; // Ajuste o valor de escala conforme necessário
 
@@ -12,8 +12,8 @@ function Drive() {
       src={scaledIframeUrl}
       frameBorder="0"
       allowFullScreen
-      title="Looker Report"
-    ></iframe>
+      title="Looker Report">
+      </iframe>
   );
 }
 
