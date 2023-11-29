25d370541b925a461e3fd792341c81c48ab60c8a
Gustavo-BDS <gugabds0@gmail.com>
2023-11-28 21:50:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
             <Sidebar />
           </div>
           <div className={`${isMenuActive ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72' : 'bg-main-bg dark:bg-main-dark-bg'
-            } w-full min-h-screen flex-2`}>
+          } w-full min-h-screen flex-2`}>
             <div className={`fixed md:static bg-main-bg ${isDarkMode ? 'dark:bg-main-dark-bg' : ''} navbar w-full`}>
               <Navbar />
             </div>
