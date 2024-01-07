9e09b0381c459028afb856e76c5dfed29437e4a2
FutuisNex <gugabds0@gmail.com>
2024-01-07 19:33:19 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
+    "jsx-a11y/label-has-associated-control": "off",
     'max-len': [
       2,
       550,
