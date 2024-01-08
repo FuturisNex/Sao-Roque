d5598a84ae792428c000bcadedcd5e53746b85dd
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-01-08 13:08:44 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
-    "jsx-a11y/label-has-associated-control": "off",
+    "jsx-a11y/control-has-associated-label": "off",
     'max-len': [
       2,
       550,
