fa02a3d165dae2db98e62131e954187d339aeac4
Gustavo <128225301+Gustavo-BDS@users.noreply.github.com>
2024-01-12 18:44:08 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useStateContext } from './contexts/ContextProvider';
-import { Navbar, Sidebar, ThemeSettings } from './components';
-import './App.css';
-import { Winthor, Rebaixa, NotFound, Encarte, Agenda, Comprador, Analise, Documentos, Sergio, Vitor, Marivone, Sena, Jurandir, RebaixaEnvio } from './pages';
+import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useStateContext } from "./contexts/ContextProvider";
+import { Navbar, Sidebar, ThemeSettings } from "./components";
+import "./App.css";
+import {
+  Winthor,
+  Rebaixa,
+  NotFound,
+  Encarte,
+  Agenda,
+  Comprador,
+  Analise,
+  Documentos,
+  Sergio,
+  Vitor,
+  Marivone,
+  Sena,
+  Jurandir,
+  RebaixaEnvio,
+} from "./pages";
 
 const App = () => {
   const { currentMode, activeMenu, themeSettings } = useStateContext();
 
-  const isDarkMode = currentMode === 'Dark';
+  const isDarkMode = currentMode === "Dark";
   const isMenuActive = activeMenu;
 
-  const isRebaixaEnvioPage = window.location.pathname === '/rebaixa-envio';
+  const isRebaixaEnvioPage = window.location.pathname === "/rebaixa-envio";
 
   return (
-    <div className={isDarkMode ? 'dark' : ''}>
+    <div className={isDarkMode ? "dark" : ""}>
       <BrowserRouter>
-        <div className={`flex relative ${isDarkMode ? 'dark:bg-main-dark-bg' : 'bg-main-bg'}`}>
-          <div className={`w-${isMenuActive && !isRebaixaEnvioPage ? '72 fixed' : '0'} sidebar ${isDarkMode ? 'dark:bg-secondary-dark-bg' : ''} bg-white`}>
+        <div
+          className={`flex relative ${
+            isDarkMode ? "dark:bg-main-dark-bg" : "bg-main-bg"
+          }`}
+        >
+          <div
+            className={`w-${
+              isMenuActive && !isRebaixaEnvioPage ? "72 fixed" : "0"
+            } sidebar ${
+              isDarkMode ? "dark:bg-secondary-dark-bg" : ""
+            } bg-white`}
+          >
             {isRebaixaEnvioPage ? null : <Sidebar />}
           </div>
-          <div className={`w-full min-h-screen flex-2 ${isMenuActive && !isRebaixaEnvioPage ? 'dark:bg-main-dark-bg bg-main-bg md:ml-72' : 'bg-main-bg dark:bg-main-dark-bg'}`}>
-            <div className={`fixed md:static bg-main-bg ${isMenuActive && !isRebaixaEnvioPage ? 'dark:bg-main-dark-bg' : ''} navbar w-full`}>
+          <div
+            className={`w-full min-h-screen flex-2 ${
+              isMenuActive && !isRebaixaEnvioPage
+                ? "dark:bg-main-dark-bg bg-main-bg md:ml-72"
+                : "bg-main-bg dark:bg-main-dark-bg"
+            }`}
+          >
+            <div
+              className={`fixed md:static bg-main-bg ${
+                isMenuActive && !isRebaixaEnvioPage
+                  ? "dark:bg-main-dark-bg"
+                  : ""
+              } navbar w-full`}
+            >
               {isRebaixaEnvioPage ? null : <Navbar />}
             </div>
             <div>
@@ -50,6 +88,7 @@ const App = () => {
         </div>
       </BrowserRouter>
       <ToastContainer />
+      <SpeedInsights />
     </div>
   );
 };
