a34d0f788392e055a6b7d9a6fdf1d0d33972ae6f
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-01-15 13:34:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
