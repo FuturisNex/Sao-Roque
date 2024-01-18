5bd9dae6d4672870d03622c73eed927ba7f6499e
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-01-18 13:15:19 -0300

    Update ListaAvarias.js

--- src/pages/Pages/Avarias/ListaAvarias.js
+++ src/pages/Pages/Avarias/ListaAvarias.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import database from '../auth/firebase';
+import database from '../../../auth/firebase';
 import "./Style/lista.css";
 
 const ListaAvarias = () => {
