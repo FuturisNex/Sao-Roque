e774b61bc8c6e7271470cbfa96dcf4977930e46d
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-01-18 21:44:23 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,9 +31,9 @@ const App = () => {
   const isDarkMode = currentMode === 'Dark';
   const isMenuActive = activeMenu;
 
-  const isRebaixaEnvioPage = 
-    window.location.pathname.startsWith('/avarias');
-    window.location.pathname.startsWith('/validades');
+  const isRebaixaEnvioPage =
+  window.location.pathname.startsWith('/avarias');
+  window.location.pathname.startsWith('/validades');
 
   return (
     <div className={isDarkMode ? 'dark' : ''}>
