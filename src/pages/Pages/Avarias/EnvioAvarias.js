18bafae49af321f89b805f1587f40881c295a0c7
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-01-22 14:19:05 -0300

    Update EnvioAvarias.js

--- src/pages/Pages/Avarias/EnvioAvarias.js
+++ src/pages/Pages/Avarias/EnvioAvarias.js
@@ -155,7 +155,6 @@ const FormPage = () => {
   const handleVlnotaChange = (event) => {
     const { name, value } = event.target;
     handleChange({ target: { name, value: value.toUpperCase() } });
-    const value = event.target.value;
     const sanitizedValue = value.replace(/[^0-9,]/g, '');
     setVlnota(sanitizedValue);
   };
