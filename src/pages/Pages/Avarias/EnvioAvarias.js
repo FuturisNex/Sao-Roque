0c5a81d1bee16d0c4c134d4fca6c8fd09bc132d4
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-01-22 12:12:14 -0300

    Update EnvioAvarias.js

--- src/pages/Pages/Avarias/EnvioAvarias.js
+++ src/pages/Pages/Avarias/EnvioAvarias.js
@@ -69,16 +69,16 @@ const FormPage = () => {
   }, [cod, previousCod]);
 
   useEffect(() => {
-    Cookies.set("responsavel", responsavel, { expires: 365 });
-    Cookies.set("filial", filial, { expires: 365 });
-    Cookies.set("comprador", comprador, { expires: 365 });
-    Cookies.set("cod", cod.toString(), { expires: 365 });
-    Cookies.set("fornecedor", fornecedor, { expires: 365 });
-    Cookies.set("tipo", tipo, { expires: 365 });
-    Cookies.set("volume", volume.toString(), { expires: 365 });
-    Cookies.set("nota", nota.toString(), { expires: 365 });
-    Cookies.set("vlnota", vlnota, { expires: 365 });
-    Cookies.set("obs", obs, { expires: 365 });
+    Cookies.set('responsavel', responsavel, { expires: 365 });
+    Cookies.set('filial', filial, { expires: 365 });
+    Cookies.set('comprador', comprador, { expires: 365 });
+    Cookies.set('cod', cod.toString(), { expires: 365 });
+    Cookies.set('fornecedor', fornecedor, { expires: 365 });
+    Cookies.set('tipo', tipo, { expires: 365 });
+    Cookies.set('volume', volume.toString(), { expires: 365 });
+    Cookies.set('nota', nota.toString(), { expires: 365 });
+    Cookies.set('vlnota', vlnota, { expires: 365 });
+    Cookies.set('obs', obs, { expires: 365 });
   }, [responsavel, filial, comprador, cod, fornecedor, tipo, volume, nota, vlnota, obs]);
 
   const handleChange = ({ target: { name, value } }) => {
