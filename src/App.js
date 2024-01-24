14c3197e8c41e4fb54c4c669c7f1e45b119bada4
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-01-24 14:34:27 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
 
   const isRebaixaEnvioPage = window.location.pathname.startsWith('/avarias')
   || window.location.pathname.startsWith('/validades');
-  || window.location.pathname.startsWith('/');
+  || window.location.pathname === '/';
 
   return (
     <div className={isDarkMode ? 'dark' : ''}>
