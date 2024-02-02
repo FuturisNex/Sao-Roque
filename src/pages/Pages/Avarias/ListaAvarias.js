b95995151f3de4deb0563a22d3191bec60531bf6
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-02-02 07:53:07 -0300

    Update ListaAvarias.js

--- src/pages/Pages/Avarias/ListaAvarias.js
+++ src/pages/Pages/Avarias/ListaAvarias.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import database from '../../../auth/firebase';
-import logo from '../../../data/img/logotipo.png';
 import './Style/lista.css';
 
 const ListaAvarias = () => {
