6313e1ed870e9920c0acb6feee68991776c5ddb4
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-02-02 11:18:53 -0300

    Update ListaAvarias.js

--- src/pages/Pages/Avarias/ListaAvarias.js
+++ src/pages/Pages/Avarias/ListaAvarias.js
@@ -98,14 +98,14 @@ const ListaAvarias = () => {
               ))}
             </select>
 
-            <label htmlFor="responsavelFilter">Responsável:</label>
+            <label htmlFor="responsavelFilter">Loja:</label>
             <select
               id="responsavelFilter"
               value={responsavelFilter}
               onChange={(e) => setResponsavelFilter(e.target.value)}
             >
               <option value="">Todos</option>
-              {avarias.map((avaria) => avaria.RESPONSAVEL).filter((value, index, self) => self.indexOf(value) === index).map((option) => (
+              {avarias.map((avaria) => avaria.FILIAL).filter((value, index, self) => self.indexOf(value) === index).map((option) => (
                 <option key={option} value={option}>
                   {option}
                 </option>
