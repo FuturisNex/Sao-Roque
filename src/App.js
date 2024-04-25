7a43efbfc7dbc126a5021035560016f4b511b5b3
Gustavo Batista <128225301+FuturisNex@users.noreply.github.com>
2024-04-25 17:26:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
