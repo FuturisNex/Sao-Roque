fb1ebb3726b697da7682194f357c3cc3a1c3f730
Gustavo <128225301+FuturisNex@users.noreply.github.com>
2024-05-28 17:20:44 -0300

    Update Home.jsx

--- src/pages/Pages/Home.jsx
+++ src/pages/Pages/Home.jsx
@@ -6,7 +6,7 @@ const ReportsPage = () => (
     <div className="report-container">
       <h2>Relatório 1</h2>
       <iframe
-        title="Relatório 1"
+        title="Rebaixa"
         width="1200"
         height="150"
         src="https://lookerstudio.google.com/embed/reporting/6dd0c967-915c-47e4-a828-4ac1f129d7bd/page/aKZnD"
@@ -16,12 +16,23 @@ const ReportsPage = () => (
       />
     </div>
     <div className="report-container">
-      <h2>Relatório 2</h2>
+      <h2>Performance Varejo</h2>
       <iframe
         title="Relatório 2"
         width="1140"
         height="541.25"
         src="https://app.powerbi.com/reportEmbed?reportId=888675be-0ffa-4f9a-8d1b-28096235db7b&autoAuth=true&ctid=94e777c0-1b94-4fdb-adef-eae087762289"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+    <div className="report-container">
+      <h2>Desenpenho Varejo</h2>
+      <iframe
+        title="Relatório 2"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=ef6fb643-90ec-4491-854b-fd072bb535d3&autoAuth=true&ctid=94e777c0-1b94-4fdb-adef-eae087762289"
         frameBorder="0"
         allowFullScreen
       />
