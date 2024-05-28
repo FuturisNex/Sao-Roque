c9be3cb01139c3924dbcf4419c36c44eadb8f2eb
Gustavo <128225301+FuturisNex@users.noreply.github.com>
2024-05-28 15:09:57 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -76,15 +76,17 @@ const Sidebar = () => {
                 key={link.name}
                 onClick={handleCloseSideBar}
                 style={({ isActive }) => ({
-                  backgroundColor: isActive ? currentColor : '',
+                  backgroundColor: isActive ? currentColor : ''
                 })}
-                className={({ isActive }) => isActive ? 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 sidebar-link active' : 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 sidebar-link'}
+                className={({ isActive }) =>
+                  isActive
+                    ? 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 sidebar-link active'
+                    : 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 sidebar-link'
+                }
                 target={link.target === '_blank' ? '_blank' : undefined}
               >
                 {link.icon}
-                <span className="capitalize sidebar-link-text">
-                  {link.name}
-                </span>
+                <span className="capitalize sidebar-link-text">{link.name}</span>
               </NavLink>
             ))}
           </div>
