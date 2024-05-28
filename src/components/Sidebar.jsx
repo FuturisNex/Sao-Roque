2e963e4d6c2708094e058f6a85beae73e4dcb8ab
Gustavo <128225301+FuturisNex@users.noreply.github.com>
2024-05-28 13:57:02 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { MdOutlineCancel } from "react-icons/md";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import logo from "../data/img/logotipo.png";
-import { links } from "../data/dummy";
-import { useStateContext } from "../contexts/ContextProvider";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import logo from '../data/img/logotipo.png';
+import { links } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { currentColor, activeMenu, setActiveMenu, screenSize } =
-    useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, screenSize } = useStateContext();
 
   const handleCloseSideBar = () => {
     if (activeMenu !== undefined && screenSize <= 900) {
@@ -24,36 +23,36 @@ const Sidebar = () => {
     <div
       className="h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 sidebar"
       style={{
-        width: "275px",
-        backgroundColor: "#fff",
-        boxShadow: "2px 0 5px rgba(0, 0, 0, 0.1)",
-        display: "flex",
-        flexDirection: "column",
-        paddingBottom: "10px",
-        overflow: "hidden",
-        transition: "all 0.3s ease",
+        width: '275px',
+        backgroundColor: '#fff',
+        boxShadow: '2px 0 5px rgba(0, 0, 0, 0.1)',
+        display: 'flex',
+        flexDirection: 'column',
+        paddingBottom: '10px',
+        overflow: 'hidden',
+        transition: 'all 0.3s ease',
       }}
     >
       <img
         src={logo}
-        alt="Logo"
+        alt='Logo'
         className="Sidebar-logo"
         style={{
-          display: "block",
-          margin: "25px auto -20px",
-          width: "60%",
+          display: 'block',
+          margin: '25px auto -20px',
+          width: '60%',
         }}
       />
       <div className="flex justify-between items-center sidebar-header">
         <Link
-          to="/"
+          to='/'
           onClick={handleCloseSideBar}
           className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
         />
-        <TooltipComponent content="Menu" position="BottomCenter">
+        <TooltipComponent content='Menu' position='BottomCenter'>
           <button
-            id="button"
-            type="button"
+            id='button'
+            type='button'
             onClick={() => setActiveMenu(!activeMenu)}
             style={{ color: currentColor }}
             className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden sidebar-close-btn"
@@ -64,7 +63,7 @@ const Sidebar = () => {
       </div>
       <div
         className="mt-10 sidebar-links"
-        style={{ marginTop: "2.5rem", padding: "0 1rem" }}
+        style={{ marginTop: '2.5rem', padding: '0 1rem' }}
       >
         {links.map((item) => (
           <div key={item.title}>
@@ -77,14 +76,14 @@ const Sidebar = () => {
                 key={link.name}
                 onClick={handleCloseSideBar}
                 style={({ isActive }) => ({
-                  backgroundColor: isActive ? currentColor : "",
+                  backgroundColor: isActive ? currentColor : '',
                 })}
                 className={({ isActive }) =>
                   isActive
-                    ? "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 sidebar-link active"
-                    : "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 sidebar-link"
+                    ? 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 sidebar-link active'
+                    : 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 sidebar-link'
                 }
-                target={link.target === "_blank" ? "_blank" : undefined}
+                target={link.target === '_blank' ? '_blank' : undefined}
               >
                 {link.icon}
                 <span className="capitalize sidebar-link-text">
