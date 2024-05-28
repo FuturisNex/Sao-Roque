7e1ff7ab6a383b48bcaa8b95ef4b5ba424dff7aa
Gustavo <128225301+FuturisNex@users.noreply.github.com>
2024-05-28 16:22:51 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -79,7 +79,7 @@ const Sidebar = () => {
                 to={`/${link.url}`}
                 key={link.name}
                 onClick={handleCloseSideBar}
-                style={getStyle(isActive)}
+                  style={({ isActive }) => getStyle(isActive)}
                 className={isActive ? 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 sidebar-link active' : 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 sidebar-link'}
                 target={link.target === '_blank' ? '_blank' : undefined}
               >
